b984210e94fead312a3285a99b799c76ed3fdf40
Bhanu1201 <bhanu@dvk.ai>
2025-01-30 11:08:51 +0530

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -20,7 +20,7 @@ const Login: React.FC = () => {
         localStorage.setItem('token', response.data.token);
 
         // Redirect to Home page immediately
-        navigate('https://client-sso-frontend.onrender.com');
+        navigate('/');
     } catch (err) {
         setError('Invalid credentials');
     }
